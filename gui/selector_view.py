e044af674289459680cdb95478b6154fd62b19ad
John Halazonetis <john.halazonetis@beyondgravity.com>
2022-07-19 09:46:57 +0200

    Decorative changes

--- gui/selector_view.py
+++ gui/selector_view.py
@@ -6,6 +6,6 @@ class SelectorView:
         self.view()
 
     def view(self):
-        with dpg.window(label="Selector Tree", tag="selector_view", autosize=True, no_close=True, no_collapse=True, no_move=True):
+        with dpg.window(label="Selector Tree", tag='selector_view', autosize=True, no_close=True, no_collapse=True, no_move=True):
             dpg.add_input_text(tag='tree_search', hint='Search')
             dpg.add_tree_node(label="tree node")
