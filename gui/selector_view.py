69678682834408a201d1bc869a67c1bf8047239f
John Halazonetis <john.halazonetis@icloud.com>
2022-07-20 06:34:33 +0200

    Search bar width is now correct

--- gui/selector_view.py
+++ gui/selector_view.py
@@ -7,5 +7,5 @@ class SelectorView:
 
     def view(self):
         with dpg.window(label="Selector Tree", tag='selector_view', autosize=True, no_close=True, no_collapse=True, no_move=True):
-            dpg.add_input_text(tag='tree_search', hint='Search')
+            dpg.add_input_text(tag='tree_search', hint='Search', width=-1)
             dpg.add_tree_node(label="tree node")
